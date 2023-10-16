4ae995713b7e0e19c1a5f551c6b7a4d50945b2a2
rosejoy1 <rose28012003@gmail.com>
2023-10-16 12:38:21 +0530

    version 3

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,38 +1,37 @@
 import React, { useState } from 'react'
-import { decrement, increment, incrementByAmount, reset } from '../redux/counterSlice'
 import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, reset, incrementByAmount } from '../redux/counterSlice'
 
 function Counter() {
-
-  const [amount,setAmount] = useState("")
-  const dispatch = useDispatch()
-  const count = useSelector((state)=>state.counterReducer.count)
-
-  const incrementByUserValue = ()=>{
-    if(amount==""){
-      alert("Please enter amount!!!")
+    const count = useSelector((state) => state.counterReducer.count)
+    const dispatch = useDispatch()
+    const [amount,setAmount] = useState("")
+    const handleIncrement = ()=>{
+        if(amount==""){
+            alert("please provide amount")
+        }else{
+            // Update state in store
+            dispatch(incrementByAmount(Number(amount)))
+        }
     }
-    else{
-      // dispatch action
-      dispatch(incrementByAmount(Number(amount)))
-    }
-  }
-  return (
-    <div style={{height:'70vh'}} className='w-100 d-flex justify-content-center align-items-center'>
-      <div className='w-25 d-flex justify-content-center align-items-center flex-column border rounded p-5'>
-        <h1 style={{fontSize:'100px'}}>{count}</h1>
-      <div className='w-100 d-flex justify-content-between align-items-center mt-5'>
-        <button className='btn btn-warning' onClick={()=>dispatch(decrement())}>Decrement</button>
-        <button className='btn btn-danger' onClick={()=>dispatch(reset())}>Reset</button>
-        <button className='btn btn-success' onClick={()=>dispatch(increment())}>Increment</button>
-      </div>
-      <div className="d-flex mt-4">
-        <input type="text" className='form-control' onChange={(e)=>setAmount(e.target.value)} placeholder='Enter amount to be incremented !!'/>
-        <button onClick={incrementByUserValue} className='btn btn-primary'>Increment By amount</button>
-      </div>
-      </div>
-    </div>
-  )
+    return (
+        <div style={{ height: "75vh" }} className='w-100 d-flex justify-content-center align-items-center'>
+
+            <div className='w-25 p-5 d-flex justify-content-center align-items-center border rounded flex-column'>
+                <h1 style={{ fontSize: "100px" }} >{count}</h1>
+                <div className='d-flex justify-content-between w-100'>
+                    <button className='btn btn-warning' onClick={()=>dispatch(decrement())}>Decrement</button>
+                    <button className='btn btn-danger' onClick={()=>dispatch(reset())}>Reset</button>
+                    <button className='btn btn-success'onClick={()=>dispatch(increment())}>Increment</button>
+                </div>
+                <div className='d-flex mt-3'>
+                    <input onChange={(e)=>setAmount(e.target.value)} type="text" className="form-control" placeholder='Enter amount to be incremented' />
+                    <button onClick={handleIncrement} className='btn btn-primary ms-2'>Increment by amount</button>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
-export default Counter
+export default Counter
