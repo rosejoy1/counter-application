2768f02e8c045a2b42815fa474c8502a0e80c260
rosejoy1 <rose28012003@gmail.com>
2023-10-27 18:24:08 +0530

    version 1

--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -1,16 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 const counterSlice = createSlice({
     name:'counter',
     initialState:{
-        count:0
+     count:0
     },
     reducers:{
         increment: (state)=>{
             state.count +=1
         },
-        decrement:(state)=>{
-            state.count -=1
+        decrement:(state)=>{   
+            state.count -=1       
         },
         reset:(state)=>{
             state.count = 0;
@@ -20,6 +19,5 @@ const counterSlice = createSlice({
         }
     }
 })
-
 export const {increment,decrement,reset, incrementByAmount} = counterSlice.actions
 export default counterSlice.reducer
